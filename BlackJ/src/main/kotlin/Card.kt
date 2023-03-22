bd965aa3a3c117e864e2be631cff5da6c8727ae4
elcrity <elcrity@naver.com>
2023-03-22 13:38:22 +0900

    정리

--- BlackJ/src/main/kotlin/Card.kt
+++ BlackJ/src/main/kotlin/Card.kt
@@ -3,3 +3,40 @@ data class Card(val suit: Suit, val rank: Rank){
         return "$suit$rank"
     }
 }
+
+enum class Rank(val value: Int) {
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
+
+    override fun toString(): String {
+        return when (this) {
+            ACE -> "A"
+            JACK -> "J"
+            QUEEN -> "Q"
+            KING -> "K"
+            else -> value.toString()
+        }
+    }
+}
+
+enum class Suit(val symbol: String) {
+    CLUBS("♣"),
+    DIAMONDS("♦"),
+    HEARTS("♥"),
+    SPADES("♠");
+
+    override fun toString(): String {
+        return symbol
+    }
+}
